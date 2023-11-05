99d9c3dbdb13ea3b25b54f13a1fa84abcc609cf6
youssef <amerzag.youssef4@gmail.com>
2023-11-05 20:23:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import data from './Emojis.json'
+import css from './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const [emojis , setemojis] = useState("")
+  const [filtreditems , setfiltreditems] = useState(data)
+
+  function searchinp (e) {
+    setemojis(e.target.value)
+
+    const filtredemojis = data.filter((emoji) => 
+    emoji.description.includes(emojis)
+    );
+
+    setfiltreditems(filtredemojis)
+  }
+
+  return(
+    <div>
+      <h1 style={{"color" : "wheat"}}>Emoji Search</h1>
+      <input type='text' id='inputsearch' value={emojis} onChange={searchinp}></input>
+      <ul id='list'>
+        {filtreditems.map((emojis , index) =>  <li key={index}>{emojis.emoji} {emojis.description}</li> )}
+      </ul>
+    </div>
+  )
+}export default App;
